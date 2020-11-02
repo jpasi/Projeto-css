e80e0c847c0f05e31a4c91d17f2dbdacecb5108c
Jesus Pasillas'git config --global user.name Jesus <jpasillas11@gmail.com>
2020-11-02 15:23:11 -0300

    Adding restrictions in form

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = document.getElementById("nome").Value
+let email = document.getElementById("email").value
+let assunto = document.getElementById("Assunto").value
+let mensagem = document.getElementById("Mensagem").value
+
+document.getElementById("form").addEventListener("submit", function(event){
+    event.preventDefault()
+})
+
